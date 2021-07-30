6e93533f904399f1db249e74feff83df422a56fa
SHan <hs04111@naver.com>
2021-07-30 12:53:22 +0900

    ESlint setup

--- src/server.js
+++ src/server.js
@@ -2,12 +2,14 @@ import express from "express";
 
 const app = express();
 
-app.set("view engine", "pug")
-app.set("views", __dirname + "/views")
-app.use("/public", express.static(__dirname + "/public"))
+app.set("view engine", "pug");
+app.set("views", `${__dirname}/views`);
+app.get("/public", express.static(`${__dirname}/public`));
 
-app.get("/", (req, res)=>{return res.render("home")})
+app.get("/", (req, res) => res.render("home"));
 
-const handleListen = () => console.log("✔Server running on http://localhost:3000")
+const handleListen = () => {
+  console.log("✔Server running on http://localhost:3000");
+};
 
 app.listen(3000, handleListen);

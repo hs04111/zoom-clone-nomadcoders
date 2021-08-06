b38527792e1d6338337b5a69cb94ac14f3559106
SHan <hs04111@naver.com>
2021-08-06 10:59:41 +0900

    socket.io installed

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import http from "http";
 import express from "express";
-import WebSocket from "ws";
+import SocketIo from "socket.io";
 
 const app = express();
 
@@ -15,39 +15,32 @@ const handleListen = () => {
   console.log("✔Server running on http://localhost:3000");
 };
 
-// 웹소켓을 올리기 위해, 서버를 코드에서 나타낸다.
 const server = http.createServer(app);
+const io = SocketIo(server);
 
-// 웹소켓 서버를 http 서버 위에 올린다. 반드시 이렇게만 해야 하는 것은 아니다.
-// 현재의 경우, view 등을 함께 사용하기 위해 이렇게 세팅한다.
-// 이제 같은 port에서 ws와 http 프로토콜을 모두 사용할 수 있다.
-const wss = new WebSocket.Server({ server });
-
-// 웹소켓에서도 마치 addEventListener처럼 콜백 함수를 이벤트에 붙일 수 있다.
-
-const sockets = [];
-
-wss.on("connection", (socket) => {
-  console.log("Connected to browser!");
-  sockets.push(socket);
-  socket["nick"] = "Anonymous";
-  socket.on("close", () => {
-    console.log("Disconnected from the browser");
-  });
-  socket.on("message", (message) => {
-    const parsedMessage = JSON.parse(message.toString("utf8"));
-    switch (parsedMessage.type) {
-      case "new_message":
-        sockets.forEach((aSocket) =>
-          aSocket.send(`${socket.nick}: ${parsedMessage.payload}`)
-        );
-        break;
-      case "nick":
-        socket["nick"] = parsedMessage.payload;
-        break;
-    }
-  });
-  socket.send("Hello!");
-});
+// const wss = new WebSocket.Server({ server });
+// const sockets = [];
+// wss.on("connection", (socket) => {
+//   console.log("Connected to browser!");
+//   sockets.push(socket);
+//   socket["nick"] = "Anonymous";
+//   socket.on("close", () => {
+//     console.log("Disconnected from the browser");
+//   });
+//   socket.on("message", (message) => {
+//     const parsedMessage = JSON.parse(message.toString("utf8"));
+//     switch (parsedMessage.type) {
+//       case "new_message":
+//         sockets.forEach((aSocket) =>
+//           aSocket.send(`${socket.nick}: ${parsedMessage.payload}`)
+//         );
+//         break;
+//       case "nick":
+//         socket["nick"] = parsedMessage.payload;
+//         break;
+//     }
+//   });
+//   socket.send("Hello!");
+// });
 
 server.listen(3000, handleListen);
